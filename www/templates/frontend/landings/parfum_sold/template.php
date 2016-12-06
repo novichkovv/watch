10ca1e8d15dbe518d02b2d42f4d1ccdea9eb231b
novichkovv <novichkovv@bk.ru>
2016-12-06 11:15:16 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/templates/frontend/landings/parfum_sold/template.php
+++ www/templates/frontend/landings/parfum_sold/template.php
@@ -16,12 +16,12 @@
 			n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
 			t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
 			document,'script','https://connect.facebook.net/en_US/fbevents.js');
-		fbq('init', '171195010018271');
+		fbq('init', '1816280065250492');
 		fbq('track', 'PageView');
 		fbq('track', 'AddToCart');
 	</script>
 	<noscript><img height="1" width="1" style="display:none"
-				   src="https://www.facebook.com/tr?id=171195010018271&ev=PageView&noscript=1"
+				   src="https://www.facebook.com/tr?id=1816280065250492&ev=PageView&noscript=1"
 		/></noscript>
 	<!-- DO NOT MODIFY -->
 	<!-- End Facebook Pixel Code -->
