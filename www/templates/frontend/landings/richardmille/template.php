7e28c31497c3ed107da6ecfc56923f5401b8d0cb
novichkovv <novichkovv@bk.ru>
2016-12-15 14:20:33 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

---
+++ www/templates/frontend/landings/richardmille/template.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<!-- saved from url=(0035)http://reals-gooods.ru/rich-mille2/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Легендарные часы Richard Mille как у Пескова</title>
+ <meta name="description" content="Купите легендарные часы Richard Mille, в которых ходит Песков по самой низкой цене в интернете">
+    <meta name="keywords" content="часы Richard Mille, часы как у Пескова, часы с черепом, купить Richard Mille">
+    
+    <meta name="viewport" content="width=480">
+    
+    <link rel="icon" href="http://reals-gooods.ru/rich-mille2/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="http://reals-gooods.ru/rich-mille2/favicon.ico" type="image/x-icon">
+    
+    <script type="text/javascript" src="<?php echo $dir; ?>js/jquery.min.js"></script>
+    <link href="<?php echo $dir; ?>css/style.css" rel="stylesheet" type="text/css">
+    <link media="all" href="<?php echo $dir; ?>css/main.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="<?php echo $dir; ?>js/count.js"></script>
+    <script type="text/javascript" src="<?php echo $dir; ?>js/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="<?php echo $dir; ?>js/init.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo $dir; ?>css/roboto.css">
+    <script src="<?php echo $dir; ?>js/jquery.js" type="text/javascript"></script>
+    <script src="<?php echo $dir; ?>js/plugins.js" type="text/javascript"></script>
+    
+    <script src="<?php echo $dir; ?>js/detect.js" type="text/javascript"></script>
+				</head>
+    <body>
+            <div class="main-wrap">
+            <div class="section block-1">
+                <span class="sale">-70%</span>
+                <div class="cost">
+                    <div class="old-cost col-2">9630р</div>
+                    <div class="new-cost col-1">2890р</div>
+                </div>
+                <div class="timer">
+                    <p class="center">До конца акции осталось:</p>
+                    <div class="countbox  clearfix"><div class="countbox-num"><div class="countbox-hours1"><span></span>1</div><div class="countbox-hours2"><span></span>2</div><div class="countbox-hours-text">часов</div></div><div class="countbox-space"></div><div class="countbox-num"><div class="countbox-mins1"><span></span>3</div><div class="countbox-mins2"><span></span>6</div><div class="countbox-mins-text">минут</div></div><div class="countbox-space"></div><div class="countbox-num"><div class="countbox-secs1"><span></span>5</div><div class="countbox-secs2"><span></span>8</div><div class="countbox-secs-text">секунд</div></div></div>
+                </div>
+                <div class="center"><a href="#order" class="button">Заказать со скидкой</a></div>
+            </div>
+            <div class="section block-2">
+                <h2>Часы <span class="col-1">Richard Mille</span><br><small>это прежде всего:</small></h2>
+                <ul>
+                    <li>
+                        <h3 class="col-1">Элитный бренд</h3>
+                        <p>Международный бренд. Всемирная популярность. Высокие продажи по всеми миру.</p>
+                    </li>
+                    <li>
+                        <h3 class="col-1">Совершенный дизайн</h3>
+                        <p>Часы Richard Mille - олицетворение статуса и успеха, чувства стиля и вкуса, оригинальности и самодостаточности. Это нечто большее, чем просто часы, это шедевры высокого часового искусства, которое зародилось в прекрасной Бельгии.</p>
+                    </li>
+                    <li>
+                        <h3 class="col-1">Высокое качество</h3>
+                        <p>Корпус часов обладает повышенной устойчивостью к коррозии и сохраняет стойкость цвета в течение многих десятков лет.</p>
+                    </li>
+                    <li>
+                        <h3 class="col-1">Мировая популярность</h3>
+                        <p>Часы Richard Mille на сегодняшний день являются показателем успеха современного мужчины. Звёзды Голливуда, шоу-бизнеса, политики и спортсмены не могут ошибаться. "Каждый уважающий себя мужчина должен носить эти Часы!"</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="line"></div>
+            <div class="section block-3">
+                <h2>Характеристики <span class="col-1">Richard Mille</span></h2>
+                <img src="<?php echo $dir; ?>img/tov-1.jpg" alt="Часы Richard Mille">
+                <div class="tov-spec">
+                    <dl>
+                        <dt>Сборка:</dt>
+                        <dd>Бельгия</dd>
+                    </dl>
+                    <dl>
+                        <dt>Форма:</dt>
+                        <dd>Бочка образная</dd>
+                    </dl>
+                    <dl>
+                        <dt>Размер:</dt>
+                        <dd>W=43мм, Н=50мм, Т=16мм</dd>
+                    </dl>
+                    <dl>
+                        <dt>Цвет браслета:</dt>
+                        <dd>черный</dd>
+                    </dl>
+                    <dl>
+                        <dt>Ремешок:</dt>
+                        <dd>Высокопрочный эластичный каучук</dd>
+                    </dl>
+                    <dl>
+                        <dt>Механизм:</dt>
+                        <dd>Кварцевый</dd>
+                    </dl>
+                    <dl>
+                        <dt>Цвет:</dt>
+                        <dd>золотой или черный</dd>
+                    </dl>
+                    <dl>
+                        <dt>Водозащита:</dt>
+                        <dd>бытовая</dd>
+                    </dl>
+                    <dl>
+                        <dt>Функции:</dt>
+                        <dd>Часовая и минутная стрелка</dd>
+                    </dl>
+                </div>
+                <div class="cost clearfix">
+                    <div class="old-cost">9630р</div>
+                    <div class="new-cost col-1">2890р</div>
+                </div>
+                <div class="center"><a href="http://reals-gooods.ru/rich-mille2/#order" class="button">Заказать со скидкой</a></div>
+            </div>
+            <div class="section block-4">
+                <h2>Доставка и <span class="col-1">оплата</span></h2>
+                <ul>
+                    <li>
+                        <h3 class="col-1">Доставка</h3>
+                        <p>Доставка почтой бандеролями 1-го класса, в течение 5-10 рабочих дней. Стоимость доставки товара от 350р.</p>
+                    </li>
+                    <li>
+                        <h3 class="col-1">Оплата</h3>
+                        <p>Никаких предоплат! Оплата заказов осуществляется непосредственно по факту получения заказа.</p>
+                    </li>
+                    <li>
+                        <h3 class="col-1">Гарантии</h3>
+                        <p>Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины возврата.</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="section block-5">
+                <h2><span class="col-1">Отзывы</span> покупателей</h2>
+                <div class="rew-cont">
+                    <div class="rew-item">
+                        <img src="<?php echo $dir; ?>img/rew-1.jpg" alt="Часы Richard Mille">
+                        <span class="col-1">АНТОН ГАВРИКОВ, г. Москва</span>
+                        <p>Я хоть уже не молод, но попал случайно на сайт и меня привлекла модель часов и их описание. Есть в них что-то эпическое и необычное…. Решил заказать. Скажу, что в реальности на вид они еще интереснее, чем на картинках. Рад, что заказал. Спасибо за быструю доставку. Мне понравились очень и даже моим знакомым, теперь кто видит, себе такие хотят.</p>
+                    </div>
+                    <div class="rew-item">
+                        <img src="<?php echo $dir; ?>img/rew-2.jpg" alt="Часы Richard Mille">
+                        <span class="col-1">АЛЕКСЕЙ ФРОЛОВ, г. Челябинск</span>
+                        <p>Всегда мечтал о таких часах – и вот они у меня!!!! ) Да ещё и крутые такие, классный ремешок (знаю, только он сколько стоит отдельно). Очень доволен. На вид просто супер! Идут отлично, минута в минуту. Спасибо!</p>
+                    </div>
+                    <div class="rew-item">
+                        <img src="<?php echo $dir; ?>img/rew-3.jpg" alt="Часы Richard Mille">
+                        <span class="col-1">ИЛЬЯ СВЕТОНОСОВ, г. Казань</span>
+                        <p>Уговорил жену подарить такие часики мне на день рождения. Купила, хотя сказала, что ей больше нравится классика. Но когда мы их получили, то она призналась, что выглядят они очень круто и несколько агрессивно:)) Вот так! Если даже моей жене понравились, то всё ОК! Можете смело покупать)))</p>
+                    </div>
+                </div>
+            </div>
+            <div class="order" id="order">ОСТАВЬТЕ ЗАЯВКУ &nbsp;<br><small>и наши менеджеры свяжутся с вами в ближайшее время</small></div>
+            <div class="section block-1 end">
+                <span class="sale">-70%</span>
+                <div class="cost">
+                    <div class="old-cost col-2">9630р</div>
+                    <div class="new-cost col-1">2890р</div>
+                </div>
+                <form action="http://reals-gooods.ru/rich-mille2/call.php" method="post" onsubmit="if(this.name.value==&#39;&#39;){alert(&#39;Введите Ваше имя&#39;);return false}if(this.phone.value==&#39;&#39;){alert(&#39;Введите Ваш номер телефона&#39;);return false}return true;">
+                    <div><input type="text" name="name" placeholder="Введите Ф.И.О" required=""></div>
+                    <div><input type="text" name="phone" placeholder="Введите телефон" required=""></div>
+                    <div><button class="button">Заказать со скидкой</button></div>
+                </form>
+            </div>
+        </div>
+        <div class="center" style="padding-bottom: 20px;">
+            <img class="mob-vis" src="<?php echo $dir; ?>img/cop1-b-320.png" alt="Часы Richard Mille">
+<p style="text-align: center"><a style="color: inherit;" href="http://reals-gooods.ru/rich-mille2/politics.html" target="_blank">Политика конфиденциальности</a></p>
+        </div>
+   </body>
+</html>
