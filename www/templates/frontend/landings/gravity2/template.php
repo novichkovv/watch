2411be396981ad7aadd5df3fc735000e3e859598
novichkovv <novichkovv@bk.ru>
2016-12-22 20:37:12 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

---
+++ www/templates/frontend/landings/gravity2/template.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<!-- saved from url=(0040)http://reals-gooods.ru/antigravitycase1/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Антигравитационный чехол для iPhone, Samsung</title>
+    
+    <meta name="viewport" content="width=480">
+    <meta name="description" content="Уникальная новинка - чехол для телефона, который крепится к любой поверхности!">
+    <meta name="keywords" content="чехол для айфона, чехол для самсунг, антигравитационный чехол, чехол для iPhone, чехол для samsung, купить чехол для телефона">
+    
+    <link rel="icon" href="http://reals-gooods.ru/antigravitycase1/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="http://reals-gooods.ru/antigravitycase1/favicon.ico" type="image/x-icon">
+
+    <link href="<?php echo $dir; ?>css/css.css" rel="stylesheet">
+    <link media="all" href="<?php echo $dir; ?>css/main.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="<?php echo $dir; ?>js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo $dir; ?>js/init.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo $dir; ?>css/roboto.css">
+    <script src="<?php echo $dir; ?>js/jquery.js" type="text/javascript"></script>
+    <script src="<?php echo $dir; ?>js/plugins.js" type="text/javascript"></script>
+    <script src="<?php echo $dir; ?>js/detect.js" type="text/javascript"></script>
+</head>
+    <body>
+            <div class="main-wrap">
+            <div class="section block-1">
+                <div class="container clearfix">
+                    <div class="models">
+                        <h4>Для смартфонов:</h4>
+                        <ul>
+                            <li>iPhone 5, 6, 6+, 7, 7+</li>
+                            <li>samsung note 5</li>
+                            <li>samsung s6</li>
+                            <li>samsung edge 6+</li>
+                            <li>samsung edge now s7</li>
+                        </ul>
+                    </div>
+                    <div class="cost">
+                        <h4>Акция:</h4>
+                        <span class="sale">-75%</span><br>
+                        <span class="old-cost">4230р</span><br>
+                        <span class="new-cost">1090р</span>
+                    </div>
+                </div>
+            </div>
+            <div class="center but-1"><a href="#delivery" class="button-m"></a></div>
+            <div class="section block-2">
+                <h2>Чехол - открытие года!</h2>
+                <div class="container">
+                    <p>Антигравитационный чехол в разы увеличивает функцинальность вашего телефона и делает его просто незамениным в повседневной жизни. Это стало возможным благодаря внедрению технологии «прилипания» - в результате чего чехол может прикрепляться к любым поверхностям, даже самым скользким! Также в составе чехла имеется специальный корпус, который отличается повышенной ударопрочностью.</p>
+                    <img src="<?php echo $dir; ?>img/bg-3.jpg" alt="Антигравитационный чехол">
+                    <p>Надежную фиксацию с ровными поверхностями обеспечивает специальный пористый материал задней крышки. При соприкосновении и небольшом надавливании он действует подобно тысячам микроскопических присосок, позволяющих удерживать устройство. Сама панелька при этом совсем не липкая, что исключает какие-либо неудобства в повседневном использовании.</p>
+                </div>
+            </div>
+            <div class="section block-3">
+                <h2>Иновационный материал</h2>
+                <div class="container">
+                    <ul>
+                        <li>Отличается повышенной ударопрочностью, не реагирует на небольшие повреждения и царапины.</li>
+                        <li>Не нарушает работы коммуникационных модулей телефона.</li>
+                        <li>Стильный и тонкий - увеличение габаритов устройства около 3 мм.</li>
+                    </ul>
+                    <div class="center"><a href="#delivery" class="button-m"></a></div>
+                </div>
+            </div>
+            <div class="section block-4">
+                <h2>К чему он может прикрепляться?</h2>
+            </div>
+            <div class="section block-7">
+                <h2>Доставка и оплата</h2>
+                <div class="container">
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-1"></span>
+                        <p>Доставка почтой, бандеролями 1-го класса, в течение 5-10 рабочих дней.<br>Стоимость доставки товара от 390р.</p>
+                    </div>
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-2"></span>
+                        <p>Никаких предоплат! Оплата заказов осуществляется непосредственно по факту получения заказа</p>
+                    </div>
+                    <div class="deliv-item clearfix">
+                        <span class="deliv-icon deliv-pic-3"></span>
+                        <p>Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины возврата.</p>
+                    </div>
+                </div>
+                <div class="center"><a href="#delivery" class="button-m"></a></div>
+            </div>
+            <!--<div class="section block-5">
+                <h2>Варианты расцветки</h2>
+            </div>-->
+            <div class="section block-1 end">
+                <div class="container clearfix">
+                    <div class="models">
+                        <h4>Для смартфонов:</h4>
+                        <ul>
+                            <li>iPhone 5, 6, 6+, 7, 7+</li>
+                            <li>samsung note 5</li>
+                            <li>samsung s6</li>
+                            <li>samsung edge 6+</li>
+                            <li>samsung edge now s7</li>
+                        </ul>
+                    </div>
+                    <div class="cost">
+                        <h4>Акция:</h4>
+                        <span class="sale">-75%</span><br>
+                        <span class="old-cost">4230р</span><br>
+                        <span class="new-cost">1090р</span>
+                    </div>
+                </div>
+                <div class="container-2" id="delivery">
+                    <div class="order">
+                        <p class="center">
+                            <strong>Оставьте заявку</strong><br>
+                            и мы свяжемся с вами для уточнения деталей&nbsp;заказа
+                        </p>
+                        <form action="<?php echo SITE_DIR; ?>order/m1/" method="post" class="m1_form">
+                            <div><input type="text" name="name" placeholder="Введите Ф.И.О" required=""></div>
+                            <div><input type="text" name="phone" placeholder="Введите телефон" required=""></div>
+                            <input type="hidden" name="visitor_id" value="<?php echo $visitor_id; ?>"/>
+                            <input type="hidden" name="product_id" value="<?php echo $product['affiliate_id']; ?>"/>
+                            <input type="hidden" name="ref" value="<?php echo $product['webmaster_id']; ?>"/>
+                            <input type="hidden" name="app_product_id" value="<?php echo $product['id']; ?>"/>
+                            <input type="hidden" name="s" value="<?php echo $_GET['s']; ?>">
+                            <input type="hidden" name="t" value="<?php echo $_GET['t']; ?>">
+                            <input type="hidden" name="w" value="<?php echo $_GET['w']; ?>">
+                            <input type="hidden" name="pixel" value="<?php echo $_GET['pixel']; ?>">
+                            <div><button class="button-m"></button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="section footer">
+                <div class="center">
+                    <img class="mob-vis" src="<?php echo $dir; ?>img/cop1-b-320.png" alt="copyright">
+<p style="text-align: center"><a style="color: inherit;" href="http://reals-gooods.ru/antigravitycase1/politics.html" target="_blank">Политика конфиденциальности</a></p><p></p>
+                </div>
+            </div>
+        </div>
+    </body></html>
