cf1e6ac2ad8cb9c49ebeeddded07f0606cbaa0d1
novichkovv <novichkovv@bk.ru>
2017-01-24 10:41:09 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/templates/frontend/landings/gravity_sold/template.php
+++ www/templates/frontend/landings/gravity_sold/template.php
@@ -18,6 +18,19 @@
     <script src="<?php echo $dir; ?>js/jquery.js" type="text/javascript"></script>
     <script src="<?php echo $dir; ?>js/plugins.js" type="text/javascript"></script>
     <script src="<?php echo $dir; ?>js/detect.js" type="text/javascript"></script>
+    <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '<?php echo ($pixel ? $pixel : '1816280065250492'); ?>');
+        fbq('track', 'PageView');
+        fbq('track', 'AddToCart');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=<?php echo ($pixel ? $pixel : '1816280065250492'); ?>&ev=PageView&noscript=1"
+        /></noscript>
 </head>
     <body>
     <div class="main-wrap">
