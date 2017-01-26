d61e55fa7a56e6807d30404e0549228d2123e718
novichkovv <novichkovv@bk.ru>
2017-01-26 10:48:32 +0300

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/templates/backend/orders/ajax/goods_table.php
+++ www/templates/backend/orders/ajax/goods_table.php
@@ -1,5 +1,7 @@
+<?php echo $sum = 0; ?>
 <?php if ($order_goods): ?>
     <?php foreach ($order_goods as $k => $good): ?>
+        <?php $sum += $good['order_price']; ?>
         <tr class="good_tr" data-id="<?php echo $k; ?>">
             <td>
                 <?php echo $good['good_name']; ?>
@@ -17,3 +19,28 @@
         </tr>
     <?php endforeach; ?>
 <?php endif; ?>
+<?php if ($product['price_delivery']): ?>
+    <?php $sum += ($order['delivery_price'] ? $order['delivery_price'] : $product['price_delivery']); ?>
+    <tr class="good_tr" data-id="100">
+        <td>
+            Доставка
+        </td>
+        <td>
+            <input name="order[delivery_price]" value="<?php echo $order['delivery_price'] ? $order['delivery_price'] : $product['price_delivery']; ?>" type="text" class="form-control">
+        </td>
+        <td>
+            <button class="btn outline red delete_good" type="button" data-id="100"><i class="fa fa-times"></i> </button>
+        </td>
+    </tr>
+<?php endif; ?>
+<tr>
+    <td>
+        Сумма
+    </td>
+    <td id="order_sum">
+        <?php echo $sum; ?>
+    </td>
+    <td>
+    </td>
+</tr>
+
