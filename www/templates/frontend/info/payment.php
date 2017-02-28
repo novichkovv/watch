e52947cb9319160a151c3582deb7ea7b6fdb7663
novichkovv <novichkovv@bk.ru>
2017-02-28 20:57:25 +0700

    No message
    Empty message
    
    Signed-off-by: novichkovv <novichkovv@bk.ru>

--- www/templates/frontend/info/payment.php
+++ www/templates/frontend/info/payment.php
@@ -10,97 +10,97 @@
         <br>При получении товара обязательно проверьте комплектацию заказа, наличие гарантийного талона и чека.</p>
 
 
-    <p><strong>Банковские карты</strong></p>
-    <p>Сервис приёма оплаты предоставлен <a href="http://payanyway.ru/">PayAnyWay</a>.</p>
-    <table border="0" cellspacing="0" cellpadding="0">
-        <tbody>
-        <tr>
-            <td width="100" valign="top">
-                <p><img src="https://www.moneta.ru/info/public/requirements/visa.png"></p>
-                <p><img src="https://www.moneta.ru/info/public/requirements/mastercard.png"></p>
-
-            </td>
-            <td valign="top">
-                <p>PayAnyWay не передает данные Вашей карты магазину и иным третьим лицам. Безопасность платежей с помощью банковских карт обеспечивается технологиями защищенного соединения HTTPS и двухфакторной аутентификации пользователя 3D Secure.</p>
-                <p>В соответствии с ФЗ «О защите прав потребителей» в случае, если Вам оказана услуга или реализован товар ненадлежащего качества, платеж может быть возвращен на банковскую карту, с которой производилась оплата. Порядок возврата средств уточняйте у администрации интернет-магазина.</p>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-
-    <p><strong>Электронные деньги</strong></p>
-    <table border="0" cellspacing="0" cellpadding="0">
-        <tbody>
-        <tr>
-            <td width="100" valign="top">
-                <p><a href="http://www.moneta.ru/" target="_blank"><img src="https://www.moneta.ru/info/public/requirements/moneta.png"></a></p>
-            </td>
-            <td valign="top">
-                <p><strong>Монета.Ру</strong>
-                    <br>Для осуществления оплаты с помощью Монета.Ру вам необходимо иметь кошелек, зарегистрировать который можно на сайте системы.
-                    <br>Способы пополнения кошелька можно найти на сайте Монета.Ру в разделе «Как пополнить». Зачисление платежей через Монета.Ру происходит мгновенно.</p>
-            </td>
-        </tr>
-
-        <tr>
-            <td width="100" valign="top">
-                <p><img src="https://www.moneta.ru/info/public/requirements/wm_transparent.png"></p>
-
-            </td>
-            <td valign="top">
-                <p><strong>WebMoney</strong>
-                    <br>Для совершения оплаты вы должны быть зарегистрированы в системе WebMoney Transfer. К оплате принимаются титульные знаки WMR, зачисление денег происходит мгновенно.</p>
-            </td>
-        </tr>
-
-        <tr>
-            <td width="100" valign="top">
-                <p><img src="https://www.moneta.ru/info/public/requirements/yandexmoney.png"></p>
-            </td>
-            <td valign="top">
-                <p><strong>Яндекс.Деньги</strong>
-                    <br>Для осуществления оплаты с помощью сервиса Яндекс.Деньги вам необходимо иметь кошелек, зарегистрированный на сайте системы.
-                    <br>Зачисление платежей через сервис Яндекс.Деньги происходит мгновенно.</p>
-            </td>
-        </tr>
-        <tr>
-            <td width="100" valign="top">
-                <p><img src="https://www.moneta.ru/info/public/requirements/qiwi.png"></p>
-            </td>
-            <td valign="top">
-                <p><strong>QIWI Кошелек</strong>
-                    <br>Выберите в качестве оплаты QIWI Кошелёк и введите номер своего сотового телефона. Оплатите созданный автоматически счёт на сайте сервиса.
-                    <br>Если у вас нет QIWI Кошелька, вам необходимо зарегистрировать его на сайте сервиса или в любом из приложений QIWI Кошелька.</p>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-
-    <p><strong>Банковские сервисы</strong></p>
-    <p>Системы онлайн-банкинга «Сбербанк ОнЛ@йн», «Альфа-Клик», «Промсвязьбанк», «Русский Стандарт», «Faktura.ru».
-        <br>Банковским или почтовым переводом, а также через систему денежных переводов «CONTACT».</p>
-    <p><img src="https://www.moneta.ru/info/public/requirements/bank.png"></p>
-
-    <p><strong>Платёжные терминалы</strong></p>
-    <p>Элекснет, ОПЛАТА.РУ, Федеральная система Город, Московский кредитный банк, Форвард Мобайл, НКО «ЛИДЕР».</p>
-    <p><img src="https://www.moneta.ru/info/public/requirements/terminal.png"></p>
-
-    <p><strong>SMS сервисы</strong></p>
-    <p>Сотовые операторы Мегафон, МТС, Beeline, Tele2</p>
-    <p><img src="https://www.moneta.ru/info/public/requirements/megafon.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/mts.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/beeline.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/tele2.png"></p>
-    <p><strong>Безналичный расчёт</strong></p>
-    <p>После оформления заказа будет сформирован счёт на оплату, который Вы можете распечатать и оплатить. Денежные средства поступят на наш счёт в течение 2-3 рабочих дней после оплаты заказа.
-        <br>Оплата заказов клиентами - юридическими лицами возможна только по безналичному расчёту.
-        <br>Все необходимые для бухгалтерии документы (оригинал счёта на оплату, счёт-фактура, накладная) выдаются вместе с заказом при получении.</p>
-
-    </br>
-    <p><strong>Возникли вопросы по оплате?</strong><br/>
-        Воспользуйтесь <a href="http://payanyway.ru/info/w/ru/public/w/payment-methods/index.html" target="_blank">подробными инструкциями</a> по каждому из способов оплаты или свяжитесь с нами.</p>
-
-    <p><strong>Служба поддержки покупателей PayAnyWay</strong></p>
-    <p><a href="http://payanyway.ru/" target="_blank"><img src="https://www.moneta.ru/info/public/requirements/payanyway.gif"></a></p>
-    <p>Телефон: +7 (495) 743-49-85
-        <br>E-mail: <a href="mailto:helpdesk.support@payanyway.ru">helpdesk.support@payanyway.ru</a></p>
+<!--    <p><strong>Банковские карты</strong></p>-->
+<!--    <p>Сервис приёма оплаты предоставлен <a href="http://payanyway.ru/">PayAnyWay</a>.</p>-->
+<!--    <table border="0" cellspacing="0" cellpadding="0">-->
+<!--        <tbody>-->
+<!--        <tr>-->
+<!--            <td width="100" valign="top">-->
+<!--                <p><img src="https://www.moneta.ru/info/public/requirements/visa.png"></p>-->
+<!--                <p><img src="https://www.moneta.ru/info/public/requirements/mastercard.png"></p>-->
+<!---->
+<!--            </td>-->
+<!--            <td valign="top">-->
+<!--                <p>PayAnyWay не передает данные Вашей карты магазину и иным третьим лицам. Безопасность платежей с помощью банковских карт обеспечивается технологиями защищенного соединения HTTPS и двухфакторной аутентификации пользователя 3D Secure.</p>-->
+<!--                <p>В соответствии с ФЗ «О защите прав потребителей» в случае, если Вам оказана услуга или реализован товар ненадлежащего качества, платеж может быть возвращен на банковскую карту, с которой производилась оплата. Порядок возврата средств уточняйте у администрации интернет-магазина.</p>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!--        </tbody>-->
+<!--    </table>-->
+<!---->
+<!--    <p><strong>Электронные деньги</strong></p>-->
+<!--    <table border="0" cellspacing="0" cellpadding="0">-->
+<!--        <tbody>-->
+<!--        <tr>-->
+<!--            <td width="100" valign="top">-->
+<!--                <p><a href="http://www.moneta.ru/" target="_blank"><img src="https://www.moneta.ru/info/public/requirements/moneta.png"></a></p>-->
+<!--            </td>-->
+<!--            <td valign="top">-->
+<!--                <p><strong>Монета.Ру</strong>-->
+<!--                    <br>Для осуществления оплаты с помощью Монета.Ру вам необходимо иметь кошелек, зарегистрировать который можно на сайте системы.-->
+<!--                    <br>Способы пополнения кошелька можно найти на сайте Монета.Ру в разделе «Как пополнить». Зачисление платежей через Монета.Ру происходит мгновенно.</p>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!---->
+<!--        <tr>-->
+<!--            <td width="100" valign="top">-->
+<!--                <p><img src="https://www.moneta.ru/info/public/requirements/wm_transparent.png"></p>-->
+<!---->
+<!--            </td>-->
+<!--            <td valign="top">-->
+<!--                <p><strong>WebMoney</strong>-->
+<!--                    <br>Для совершения оплаты вы должны быть зарегистрированы в системе WebMoney Transfer. К оплате принимаются титульные знаки WMR, зачисление денег происходит мгновенно.</p>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!---->
+<!--        <tr>-->
+<!--            <td width="100" valign="top">-->
+<!--                <p><img src="https://www.moneta.ru/info/public/requirements/yandexmoney.png"></p>-->
+<!--            </td>-->
+<!--            <td valign="top">-->
+<!--                <p><strong>Яндекс.Деньги</strong>-->
+<!--                    <br>Для осуществления оплаты с помощью сервиса Яндекс.Деньги вам необходимо иметь кошелек, зарегистрированный на сайте системы.-->
+<!--                    <br>Зачисление платежей через сервис Яндекс.Деньги происходит мгновенно.</p>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!--        <tr>-->
+<!--            <td width="100" valign="top">-->
+<!--                <p><img src="https://www.moneta.ru/info/public/requirements/qiwi.png"></p>-->
+<!--            </td>-->
+<!--            <td valign="top">-->
+<!--                <p><strong>QIWI Кошелек</strong>-->
+<!--                    <br>Выберите в качестве оплаты QIWI Кошелёк и введите номер своего сотового телефона. Оплатите созданный автоматически счёт на сайте сервиса.-->
+<!--                    <br>Если у вас нет QIWI Кошелька, вам необходимо зарегистрировать его на сайте сервиса или в любом из приложений QIWI Кошелька.</p>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!--        </tbody>-->
+<!--    </table>-->
+<!---->
+<!--    <p><strong>Банковские сервисы</strong></p>-->
+<!--    <p>Системы онлайн-банкинга «Сбербанк ОнЛ@йн», «Альфа-Клик», «Промсвязьбанк», «Русский Стандарт», «Faktura.ru».-->
+<!--        <br>Банковским или почтовым переводом, а также через систему денежных переводов «CONTACT».</p>-->
+<!--    <p><img src="https://www.moneta.ru/info/public/requirements/bank.png"></p>-->
+<!---->
+<!--    <p><strong>Платёжные терминалы</strong></p>-->
+<!--    <p>Элекснет, ОПЛАТА.РУ, Федеральная система Город, Московский кредитный банк, Форвард Мобайл, НКО «ЛИДЕР».</p>-->
+<!--    <p><img src="https://www.moneta.ru/info/public/requirements/terminal.png"></p>-->
+<!---->
+<!--    <p><strong>SMS сервисы</strong></p>-->
+<!--    <p>Сотовые операторы Мегафон, МТС, Beeline, Tele2</p>-->
+<!--    <p><img src="https://www.moneta.ru/info/public/requirements/megafon.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/mts.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/beeline.png">&nbsp;<img src="https://www.moneta.ru/info/public/requirements/tele2.png"></p>-->
+<!--    <p><strong>Безналичный расчёт</strong></p>-->
+<!--    <p>После оформления заказа будет сформирован счёт на оплату, который Вы можете распечатать и оплатить. Денежные средства поступят на наш счёт в течение 2-3 рабочих дней после оплаты заказа.-->
+<!--        <br>Оплата заказов клиентами - юридическими лицами возможна только по безналичному расчёту.-->
+<!--        <br>Все необходимые для бухгалтерии документы (оригинал счёта на оплату, счёт-фактура, накладная) выдаются вместе с заказом при получении.</p>-->
+<!---->
+<!--    </br>-->
+<!--    <p><strong>Возникли вопросы по оплате?</strong><br/>-->
+<!--        Воспользуйтесь <a href="http://payanyway.ru/info/w/ru/public/w/payment-methods/index.html" target="_blank">подробными инструкциями</a> по каждому из способов оплаты или свяжитесь с нами.</p>-->
+<!---->
+<!--    <p><strong>Служба поддержки покупателей PayAnyWay</strong></p>-->
+<!--    <p><a href="http://payanyway.ru/" target="_blank"><img src="https://www.moneta.ru/info/public/requirements/payanyway.gif"></a></p>-->
+<!--    <p>Телефон: +7 (495) 743-49-85-->
+<!--        <br>E-mail: <a href="mailto:helpdesk.support@payanyway.ru">helpdesk.support@payanyway.ru</a></p>-->
 
     <p><strong>Реквизиты для безналичной оплаты:</strong></p>
     <p>ИП Глазырина Анна Эдуардовна
